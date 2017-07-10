9d1821837c9d924a49adcd8fd78587cab1287918
Paulina Lei <paulina.y.lei@gmail.com>
2017-07-10 09:58:38 -0700

    made document ready before loading isotope

--- _includes/js/grid.js
+++ _includes/js/grid.js
@@ -1,22 +1,24 @@
 <script src="https://unpkg.com/imagesloaded@4/imagesloaded.pkgd.min.js"></script>
 <script>
-// init Isotope
-var $grid = $('.grid').isotope({
-  // options
-  itemSelector: '.grid-item',
-  percentPosition: true,
-  masonry: {
-    // set to the element
-    columnWidth: '.grid-sizer'
-  }
-});
+$(document).ready(function() {
+  // init Isotope
+  var $grid = $('.grid').isotope({
+    // options
+    itemSelector: '.grid-item',
+    percentPosition: true,
+    masonry: {
+      // set to the element
+      columnWidth: '.grid-sizer'
+    }
+  });
 // layout Isotope after each image loads
-$grid.imagesLoaded().progress( function() {
-  $grid.isotope('layout');
-});
+  $grid.imagesLoaded().progress( function() {
+    $grid.isotope('layout');
+  });
 // filter items on button click
-$('.filter-button-group').on( 'click', 'button', function() {
-  var filterValue = $(this).attr('data-filter');
-  $grid.isotope({ filter: filterValue });
+  $('.filter-button-group').on( 'click', 'button', function() {
+    var filterValue = $(this).attr('data-filter');
+    $grid.isotope({ filter: filterValue });
+  });
 });
 </script>
